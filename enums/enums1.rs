fa803c0b7df36aaca768aee415ff4e64816debba
Harlski <harley.jones1992@gmail.com>
2024-01-05 10:42:28 +1100

    Complete up to Strings

--- enums/enums1.rs
+++ enums/enums1.rs
@@ -2,16 +2,35 @@
 //
 // No hints this time! ;)
 
-// I AM NOT DONE
 
 #[derive(Debug)]
 enum Message {
+        Quit,
+        Echo(String),
+        Move,
+        ChangeColor
     // TODO: define a few types of messages as used below
 }
 
 fn main() {
     println!("{:?}", Message::Quit);
-    println!("{:?}", Message::Echo);
+    // println!("{:?}", Message::Echo);
     println!("{:?}", Message::Move);
     println!("{:?}", Message::ChangeColor);
+
+    let msg = Message::Echo("Way".to_string());
+    match msg {
+        Message::Echo(msg) => {
+            println!("This is the {}", msg);
+        }
+        Message::Quit => {
+            return
+        }
+        Message::Move => {
+            return
+        }
+        Message::ChangeColor => {
+            return
+        }
+    }
 }
--- enums/enums2.rs
+++ enums/enums2.rs
@@ -3,10 +3,12 @@
 // Execute `rustlings hint enums2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 enum Message {
+    Move {x: i32, y:i32},
+    Echo(String),
+    ChangeColor(i32, i32, i32),
+    Quit
     // TODO: define the different variants used below
 }
 
@@ -14,12 +16,14 @@ impl Message {
     fn call(&self) {
         println!("{:?}", self);
     }
+
 }
 
 fn main() {
+    let the_message = "hello world";
     let messages = [
         Message::Move { x: 10, y: 30 },
-        Message::Echo(String::from("hello world")),
+        Message::Echo(String::from(the_message)), // Just tested around with String::from to understand it a bit better
         Message::ChangeColor(200, 255, 255),
         Message::Quit,
     ];
